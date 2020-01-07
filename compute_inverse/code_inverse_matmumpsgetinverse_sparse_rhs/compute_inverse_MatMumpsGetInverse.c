66222b6ecfa71ade9ce36bd7e20b62b916c20ad7
jg1080@uni-freiburg.de <fr_jg1080@login1.nemo.privat>
2020-01-07 13:15:21 +0100

    Enabled possibility to not use a square block of entries of the inverse to compute but rather a rectangular block.

--- compute_inverse/code_inverse_matmumpsgetinverse_sparse_rhs/compute_inverse_MatMumpsGetInverse.c
+++ compute_inverse/code_inverse_matmumpsgetinverse_sparse_rhs/compute_inverse_MatMumpsGetInverse.c
@@ -1,14 +1,17 @@
 static char help[] ="Compute a part of the inverse of a sparse matrix. This code requires that PETSc was configured with MUMPS since we are dealing with large matrices \
 		     and therefore use a parallel LU factorization. Furthermore computation of selected entries in inv(A) is done using MatMumpsGetInverse.\
 		     Note that the number of columns nrhs of the inverse can be chosen smaller than the number of columns N in A. Therefore only a part of the inverse is computed.\n \
- 		     We compute only a the part of the matrix in the columns/rows for (0,nrhs)/(0,nrhs).  \n\
+ 		     We compute only a the part of the matrix in the columns/rows for (0,rhs_col)/(0,rhs_row).  \n\
 		     Input parameters: \n\
   			-fin <input_file> : file to load \n \
                 	-fout <input_file> : file to load \n \
-			-nrhs <numberofcolumns> : Number of columns to compute \n \
+			-rhs_col <numberofcolumns> : Number of columns to compute \n \
+                        -rhs_row <numberofrows> : Number of rows to compute \n \
                         -displ <Bool>: Print matrices to terminal \n\
 		     Example usage: \n \
-		          mpiexec -np 2 ./compute_inverse_MatMumpsGetInv -fin ../../convert_to_binary_petsc_matrix/main_off_diagonal_matrix_offValue2_ncols10_full/main_off_diagonal_matrix_offValue2_ncols10 -fout inverse_main_off_diagonal_matrix_offValue2_ncols10 -nrhs 5 -displ";
+		          mpiexec -np 2 ./compute_inverse_MatMumpsGetInv -fin ../../convert_to_binary_petsc_matrix/main_off_diagonal_matrix_offValue2_ncols10_full/main_off_diagonal_matrix_offValue2_ncols10 -fout inverse_main_off_diagonal_matrix_offValue2_ncols10 -rhs_row 5 -rhs_col 5 -displ \n\
+		     Debugging: \n \
+		         Prifiling is done by using -log_view";
 
 #include <stdio.h>
 #include <petscmat.h>
@@ -20,9 +23,10 @@ int main(int argc, char **args){
 #if defined(PETSC_HAVE_MUMPS)
     Mat			A,F,spRHS;				// Abstract PETSc matrix object used to manage all linear operators in PETSc
     PetscViewer		fd; 					// Abstract PETSc object that helps view (in ASCII, binary, graphically etc) other PETSc objects
-    PetscBool      	flg1,flg2;				// Logical variable. Actually an int in C.
+    PetscBool      	flg1,flg2,flg3;				// Logical variable. Actually an int in C.
     PetscBool		displ=PETSC_FALSE;			// Display matrices if set to True otherwise False
-    PetscInt		M,N,m,n,rstart,rend,nrhs,i,j;		// PETSc type that represents an integer, used primarily to represent size of arrays and indexing into arrays.
+    PetscInt		M,N,m,n,rstart,rend,i,j;		// PETSc type that represents an integer, used primarily to represent size of arrays and indexing into arrays.
+    PetscInt		rhs_col,rhs_row;			// Number of columns and rows on RHS (right-hand side)
     char		inputfile[1][PETSC_MAX_PATH_LEN]; 	// Input file name 
     char		outputfile[1][PETSC_MAX_PATH_LEN]; 	// Outputfile file name 
 #endif
@@ -50,10 +54,11 @@ int main(int argc, char **args){
     ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
     ierr = MatSetType(A, MATAIJ);CHKERRQ(ierr);  
     ierr = MatLoad(A, fd);CHKERRQ(ierr);
+
     // Print matrix A if -displ is set. 
     if (displ){
         ierr = PetscPrintf(PETSC_COMM_WORLD, "\n---------------\n");CHKERRQ(ierr);
-        ierr = PetscPrintf(PETSC_COMM_WORLD,"Matrix A from file:\n", nrhs);
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Matrix A from file:\n");
         ierr = MatView(A, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "\n");CHKERRQ(ierr);
     }
@@ -70,9 +75,11 @@ int main(int argc, char **args){
     ierr = PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT);CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD, "---------------\n");CHKERRQ(ierr);
 
-    // We are intersted only in a part of the inverse especially in the columns/rows for (0,nrhs)/(0,nrhs)
-    nrhs = N;
-    ierr = PetscOptionsGetInt(NULL, NULL, "-nrhs", &nrhs, &flg2);CHKERRQ(ierr);
+    // We are intersted only in a part of the inverse especially in the columns/rows for (0,rhs_col)/(0,rhs_row)
+    rhs_col = N;
+    rhs_row = M;
+    ierr = PetscOptionsGetInt(NULL, NULL, "-rhs_col", &rhs_col, &flg2);CHKERRQ(ierr);
+    ierr = PetscOptionsGetInt(NULL, NULL, "-rhs_row", &rhs_row, &flg3);CHKERRQ(ierr);
         
     // Create SpRHST for inv(A) with sparse RHS stored in the host.
     // PETSc does not support compressed column format which is required by MUMPS for sparse RHS matrix and furthermore MUMPS requires nrhs=N
@@ -89,16 +96,18 @@ int main(int argc, char **args){
     ierr = MatSetFromOptions(spRHS);CHKERRQ(ierr);
     ierr = MatSetUp(spRHS);CHKERRQ(ierr);
     if (!rank){
-        PetscScalar v[nrhs];
-        PetscInt idxn[nrhs];
-        // Generate the column indices 
-        for(j = 0; j < nrhs; j++){
+        // Value to fill into the matrix 
+        PetscScalar v[rhs_col];
+        // Global column indices
+        PetscInt idxn[rhs_col];
+        // Fill value array and create indices of columns and rows
+        for(j = 0; j < rhs_col; j++){
+            v[j] = 1.0; 
             idxn[j] = j;
-            v[j] = 1.0;
         }  
         // Fill the upper left part of the matrix with 1. Indicates that these entries of the inverse are computed.
-        for(i=0;i<nrhs;i++){
-            ierr = MatSetValues(spRHS,1,&i,nrhs,idxn,v,INSERT_VALUES);CHKERRQ(ierr);     
+        for(i=0;i<rhs_row;i++){
+            ierr = MatSetValues(spRHS,1,&i,rhs_col,idxn,v,INSERT_VALUES);CHKERRQ(ierr);     
         }
     }
     ierr = MatAssemblyBegin(spRHS,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
@@ -107,13 +116,13 @@ int main(int argc, char **args){
     // Print matrix spRHST 
     if (displ){
         ierr = PetscPrintf(PETSC_COMM_WORLD, "\n---------------\n");CHKERRQ(ierr);
-        ierr = PetscPrintf(PETSC_COMM_WORLD,"Matrix spRHS:\n", nrhs);
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"Matrix spRHS:\n");CHKERRQ(ierr);
         ierr = MatView(spRHS, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "\n");CHKERRQ(ierr);
     }
 
     // Print information
-    ierr = PetscPrintf(PETSC_COMM_WORLD, "\nCompute %i columns and rows of the inverse using LU-factorization in MUMPS!\n", nrhs);
+    ierr = PetscPrintf(PETSC_COMM_WORLD, "\nCompute %i columns and %i rows of the inverse using LU-factorization in MUMPS!\n", rhs_col, rhs_row);
 
     // Factorize the Matrix using a parallel LU factorization in MUMPS
     ierr = MatGetFactor(A, MATSOLVERMUMPS, MAT_FACTOR_LU, &F);CHKERRQ(ierr);
@@ -136,7 +145,7 @@ int main(int argc, char **args){
 
     if (displ){
         ierr = PetscPrintf(PETSC_COMM_WORLD, "\n---------------\n");CHKERRQ(ierr);
-        ierr = PetscPrintf(PETSC_COMM_WORLD,"First %D columns of inv(A) with sparse RHS:\n", nrhs);
+        ierr = PetscPrintf(PETSC_COMM_WORLD,"First %D columns and %D rows of inv(A) with sparse RHS:\n", rhs_col, rhs_row);
         ierr = MatView(spRHS,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
         ierr = PetscPrintf(PETSC_COMM_WORLD, "---------------\n");CHKERRQ(ierr);
     }
